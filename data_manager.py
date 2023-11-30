6c604d21fcdf3e796af91459251b23492b8d8792
dadapera <davide.perozzi98@gmail.com>
2023-11-30 15:26:30 +0100

    prompt update, data_manager and query improved

--- data_manager.py
+++ data_manager.py
@@ -1,22 +1,18 @@
 import pandas as pd
 
-file_path = 'data/training_test.csv'
-df = pd.read_csv(file_path, sep=";")
+def load_data(file_path, chunk_size):
 
-df = df.drop(columns=["url","brand","valuePackage","basePrice","price","img","code"])
+    csv_reader = pd.read_csv(file_path, sep=";", chunksize=chunk_size)
 
-df['query_text'] = df.apply(lambda row: '/'.join(map(str, row)), axis=1)
+    return csv_reader
+    
 
-random_rows = df.sample(n=10)
-
-for i,r in zip(random_rows.index,random_rows["query_text"]):
-    print(i,r)
+def save_df_as_csv(df, file_path):
+    df.to_csv(file_path, sep=';')
 
 
-'''print(
-    {
-        "id": df.index[0],
-        "product": df.loc[0]['query_text']
-    }
-    )
-'''
+def append_df_to_csv(df_new, csv_existing):
+    df_existing = pd.read_csv(csv_existing, sep=';')
+    df_combined = pd.concat([df_existing, df_new], ignore_index=True)
+    df_combined.to_csv(csv_existing, index=False, sep=';')
+
